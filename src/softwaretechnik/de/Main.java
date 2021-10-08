94169397ed264f45d432e1bd93330ec2b483bafe
Jana Apfelhofer <u34975@hs-harz.de>
2021-10-08 16:21:22 +0200

    Test test test 5

--- src/softwaretechnik/de/Main.java
+++ src/softwaretechnik/de/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	/*
-	test test test testtes
+	test test test testtes ja geril
 	 */
     }
 }
--- src/softwaretechnik/de/klaseeb.java
+++ src/softwaretechnik/de/klaseeb.java
@@ -1,7 +1,7 @@
 package softwaretechnik.de;
 
 public class klaseeb {
-    int i=1;
+    int i=1111111111;
     /**
      * test test test test test
      */
